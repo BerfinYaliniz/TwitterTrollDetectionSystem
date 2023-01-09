bde4753d5d7d1e420dfec41db6c165b0fd3ac88d
MUHAMMET YILDIRIM <muhammet.safir@gmail.com>
2023-01-10 01:23:02 +0300

    Finished app

--- README.md
+++
@@ -1,4 +0,0 @@
-# TwitterTrollDetectionSystem
-![image](https://user-images.githubusercontent.com/56268192/204135267-846d5fe9-34cc-4b84-bfbe-f9a86d1fcb5a.png)
-
-![image](https://user-images.githubusercontent.com/56268192/204136069-3ea0463a-4f83-448a-bba4-6bea0ed24bd7.png)
--- app.py
+++ app.py
@@ -1,10 +1,15 @@
+import pandas as pd
 from flask import Flask, render_template, request
-from trol.hashtag import hashtag
-from tweepy import tweet
+from pandas import read_csv
+from flask import Flask, render_template, url_for
+from decisionHashtag import decisionHashtag
+from decisionHesap import decisionHesap
+from multinomialHashtag import multinomialHashtag
+from multinomialHesap import multinomialHesap
+from randomForestHashtag import randomForestHashtag
+from randomForestHesap import randomForestHesap
 
-from machinelearning import makineogren
-
-app = Flask(__name__)
+app = Flask(__name__, static_folder="static")
 
 
 @app.route('/')
@@ -15,23 +20,35 @@ def index():
 @app.route('/searchpanel', methods=['POST', 'GET'])
 def mak_logic():
     keyword = request.form.get('keyword')
-    sa = makineogren()
-    predm, keyword1, follower, following, url, name, img, bg_image = sa.DownloadData(keyword)
+    sa = multinomialHesap()
+    predm, keyword1, follower, following, url, name, img, bg, = sa.DownloadData(keyword)
     return render_template('result.html', keyword=keyword1, pred=predm, follower=follower, following=following,
                            url=url,
-                           name=name, img=img, bg_image=bg_image)
+                           name=name, img=img, bg=bg)
 
 
+@app.route('/')
 @app.route('/hashtag', methods=['POST', 'GET'])
 def hashtagy():
     words = request.form.get('words')
-    say = hashtag()
-    predm, words, follower, following, url, name, img, bg_image = say.scrape(words)
-    return render_template('hashtagresult.html', words=words, predm=predm, follower=follower, following=following,
-                           url=url, name=name, img=img, bg_image=bg_image)
+    say = multinomialHashtag()
+    name = say.scrape(words)
+    df = pd.read_csv('data/sonsonuc.csv')
+
+    result = df[["username", "text", "followers", "following", "troldurum"]]
+    result2 = result.rename(columns={"username": "Kullanıcı Adı", "text": "Tweet", "followers": "Takipçi Sayısı",
+                                     "following": "Takip Edilen Kİşi Sayısı", "troldurum": "Trol Durumu"})
+
+    return render_template('hashtagresult.html', tables=[result2.to_html()],
+                           titles=['İlgili Konu Hakkında Konuşan Hesaplar'], name=name)
 
 
+@app.errorhandler(Exception)
+def page_not_found(e):
+    print(e)
+    error = "Something went wrong, we are checking it! Click here to close "
+    return render_template("searchpanel.html", error=error)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
